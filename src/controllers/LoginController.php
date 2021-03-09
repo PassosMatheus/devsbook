56466b72494aec9036e44e5a425e3eaf268e9a51
Matheus Passos <matheus@delgrande.com.br>
2021-03-09 15:16:21 -0300

    Alterado localidade de alguns arquivos de modelo. Adicionado rotas no 'routes.php'.

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+    
+    public function signin() {
+        $this->render('login');
+    }
+
+    public function signinAction() {
+        echo "login recebido";
+    }
+
+    public function signup() {
+        echo 'cadastro';
+    }
+}
