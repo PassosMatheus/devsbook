2ed3713c1a4333d8236c901a49eb8031e6ec2ef1
Matheus Passos <matheus@delgrande.com.br>
2021-03-11 16:51:04 -0300

    Criado arquivo para selecionar o que sera mostrado no feed

--- src/handlers/Posthandler.php
+++ src/handlers/Posthandler.php
@@ -2,11 +2,13 @@
 namespace src\handlers;
 
 use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelation;
 
 class PostHandler {
     public static function addPost($idUser, $type, $body) {
-        if(!empty($idUser)) {
-            
+        if(!empty($idUser) && !empty($body)) {
+            $body = trim($body);
             Post::insert([
                 'id_user' => $idUser,
                 'type' => $type,
@@ -16,6 +18,15 @@ class PostHandler {
         }
     }
 
+    public static function getHomeFeed($idUser) {
+        //pegar lista de usuarios que EU sigo
+        $usersList = UserRelation::select()->where('user_from', $idUser)->get();
+        $users = [];
+        foreach($usersList as $userItem) {
+            $users [] = $userItem['user_to'];
+        }
+        $users[] = $idUser;
+    }
 
 }
 
