8e13ec56c677a128521a47a210f43b1f24db6c35
Matheus Passos <matheus@delgrande.com.br>
2021-03-12 17:33:37 -0300

    Criado listagem de 'seguidores', 'seguindo' e 'fotos'

--- src/views/pages/profile.php
+++ src/views/pages/profile.php
@@ -19,15 +19,15 @@
                             </div>
                             <div class="profile-info-data row">
                                 <div class="profile-info-item m-width-20">
-                                    <div class="profile-info-item-n">129</div>
+                                    <div class="profile-info-item-n"><?=count($user->followers);?></div>
                                     <div class="profile-info-item-s">Seguidores</div>
                                 </div>
                                 <div class="profile-info-item m-width-20">
-                                    <div class="profile-info-item-n">363</div>
+                                    <div class="profile-info-item-n"><?=count($user->following);?></div>
                                     <div class="profile-info-item-s">Seguindo</div>
                                 </div>
                                 <div class="profile-info-item m-width-20">
-                                    <div class="profile-info-item-n">12</div>
+                                    <div class="profile-info-item-n"><?=count($user->photos);?></div>
                                     <div class="profile-info-item-s">Fotos</div>
                                 </div>
                             </div>
@@ -69,7 +69,7 @@
                         <div class="box-header m-10">
                             <div class="box-header-text">
                                 Seguindo
-                                <span>(363)</span>
+                                <span><?=count($user->following);?></span>
                             </div>
                             <div class="box-header-buttons">
                                 <a href="">ver todos</a>
@@ -77,82 +77,21 @@
                         </div>
                         <div class="box-body friend-list">
                             
+                        <?php for($q=0;$q<9;$q++): ?>
+                            <?php if(isset($user->following[$q])): ?>
+                            
                             <div class="friend-icon">
-                                <a href="">
+                                <a href="<?=$base;?>/perfil/<?=$user->id;?>">
                                     <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
+                                        <img src="<?=$base;?>/media/avatars/<?=$user->avatar;?>" />
                                     </div>
                                     <div class="friend-icon-name">
-                                        Bonieky
+                                        <?=$follower->name;?>
                                     </div>
                                 </a>
                             </div>
-
-                            <div class="friend-icon">
-                                <a href="">
-                                    <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
-                                    </div>
-                                    <div class="friend-icon-name">
-                                        Bonieky
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div class="friend-icon">
-                                <a href="">
-                                    <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
-                                    </div>
-                                    <div class="friend-icon-name">
-                                        Bonieky
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div class="friend-icon">
-                                <a href="">
-                                    <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
-                                    </div>
-                                    <div class="friend-icon-name">
-                                        Bonieky
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div class="friend-icon">
-                                <a href="">
-                                    <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
-                                    </div>
-                                    <div class="friend-icon-name">
-                                        Bonieky
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div class="friend-icon">
-                                <a href="">
-                                    <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
-                                    </div>
-                                    <div class="friend-icon-name">
-                                        Bonieky
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div class="friend-icon">
-                                <a href="">
-                                    <div class="friend-icon-avatar">
-                                        <img src="media/avatars/avatar.jpg" />
-                                    </div>
-                                    <div class="friend-icon-name">
-                                        Bonieky
-                                    </div>
-                                </a>
-                            </div>
+                            <?php endif; ?>
+                        <?php endfor; ?>
 
                         </div>
                     </div>
@@ -164,7 +103,7 @@
                         <div class="box-header m-10">
                             <div class="box-header-text">
                                 Fotos
-                                <span>(12)</span>
+                                <span><?=count($user->photos);?></span>
                             </div>
                             <div class="box-header-buttons">
                                 <a href="">ver todos</a>
