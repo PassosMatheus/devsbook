5661831ad4e2d75f4c545196f21f8e1b437fcaac
Matheus Passos <matheus@delgrande.com.br>
2021-03-12 17:06:07 -0300

    Adicionado a pagina 'perfil'

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -2,7 +2,7 @@
 namespace src\controllers;
 
 use \core\Controller;
-use \src\handlers\LoginHandler;
+use \src\handlers\UserHandler;
 use \src\handlers\PostHandler;
 
 class ProfileController extends Controller {
@@ -10,15 +10,28 @@ class ProfileController extends Controller {
     private $loggedUser;
 
     public function __construct() {
-        $this->loggedUser = LoginHandler::checkLogin();
+        $this->loggedUser = UserHandler::checkLogin();
         if($this->loggedUser === false) {
             $this->redirect('/login'); 
         }
     }
 
     public function index($atts = []) {
+        $id = $this->loggedUser->id;
+
+        if(!empty($atts['id'])) {
+            $id = $atts['id'];
+        }
+
+        $user = UserHandler::getUser($id);
+
+        if(!$user){
+            $this->redirect('/');
+        } 
+
         $this->render('profile', [
-            'loggedUser' => $this->loggedUser
+            'loggedUser' => $this->loggedUser,
+            'user' => $user
         ]);
     }
 
