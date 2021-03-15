4d78052fac0b3a63fbcaa64b7f41a396ca08810f
Matheus Passos <matheus@delgrande.com.br>
2021-03-15 16:24:31 -0300

    Adicionado a funcao de 'seguir'

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -20,14 +20,14 @@ class ProfileController extends Controller {
     public function index($atts = []) {
         $page = intval(filter_input(INPUT_GET, 'page'));
 
+        //Detectando o usuário acessado
         $id = $this->loggedUser->id;
-
         if(!empty($atts['id'])) {
             $id = $atts['id'];
         }
 
+        //Pegando iformaçoes do usuário
         $user = UserHandler::getUser($id, true);
-
         if(!$user){
             $this->redirect('/');
         } 
@@ -36,16 +36,37 @@ class ProfileController extends Controller {
         $dateTo = new \DateTime('today');
         $user->ageYears =  $dateFrom->diff($dateTo)->y;
 
+        //Pegando o feed do usuário
         $feed = PostHandler::getUserFeed(
             $id, 
             $page, 
             $this->loggedUser->id);
 
+        //Verificar se EU sigo o usuário
+        $isFollowing = false;
+        if($user->id != $this->loggedUser->id) {
+            $isFollowing = UserHandler::isFollowing($this->loggedUser->id, $user->id);
+        }
+
+
         $this->render('profile', [
             'loggedUser' => $this->loggedUser,
             'user' => $user,
-            'feed' => $feed
+            'feed' => $feed,
+            'isFollowing' => $isFollowing
         ]);
     }
 
+    public function follow ($atts) {
+        $to = intval($atts['id']);
+
+        if(UserHandler::idExists($to)) {
+            if(UserHandler::isFollowing($this->loggedUser->id, $to)) {
+                UserHandler::unfollow($this->loggedUser->id, $to);
+            } else {
+                UserHandler::follow($this->loggedUser->id, $to);
+            }
+        } 
+        $this->redirect('/perfil/'.$to);
+    }
 }
