258c2f991c566393bfe06a957b3908f75e6085ef
Matheus Passos <matheus@delgrande.com.br>
2021-03-16 16:04:24 -0300

    Adicionado campo de busca

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -9,8 +9,6 @@
         <div class="row">
             <div class="column pr-5">
  
-                <?=$render('feed-editor', ['user'=>$loggedUser]);?>
-
                 <?php foreach($feed['posts'] as $feedItem): ?>
                     <?=$render('feed-item', [
                         'data' => $feedItem,
@@ -26,23 +24,7 @@
 
                 </div>
                 <div class="column side pl-5">
-                    <div class="box banners">
-                        <div class="box-header">
-                            <div class="box-header-text">Patrocinios</div>
-                            <div class="box-header-buttons">
-                                
-                            </div>
-                        </div>
-                        <div class="box-body">
-                            <a href=""><img src="https://alunos.b7web.com.br/media/courses/php-nivel-1.jpg" /></a>
-                            <a href=""><img src="https://alunos.b7web.com.br/media/courses/laravel-nivel-1.jpg" /></a>
-                        </div>
-                    </div>
-                    <div class="box">
-                        <div class="box-body m-10">
-                            Criado com ❤️ por B7Web
-                        </div>
-                    </div>
+                   <?=$render('rigth-side');?>
                 </div>
             </div>
         </section>
